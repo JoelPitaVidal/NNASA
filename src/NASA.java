83e764aa7d193e5d0e1ac83ecf51d7c69ae7eb70
JoelPitaVidal <jpitavidal@daielcastelao.org>
2024-04-24 19:02:18 +0200

    Simulacion de trayectoria finalizada

--- src/NASA.java
+++ src/NASA.java
@@ -11,12 +11,13 @@ import javax.swing.*;
 import java.io.*;
 
 public class NASA extends JFrame {
-            int opcion = 0;
-            ArrayList<String> misiones= new ArrayList<>();
-             Reportes r = new Reportes();
+    int opcion = 0;
+    ArrayList<String> misiones = new ArrayList<>();
+    Reportes r = new Reportes();
+
     public class Layout extends JFrame {
 
-        public Layout(){
+        public Layout() {
             //TODO: cambiar a inglés los textos
             //Configuramos el titulo del Layout y el cierre del mismo
             setTitle("Bienvenido a la interfaz de gestión de misiones de la NASA");
@@ -41,22 +42,22 @@ public class NASA extends JFrame {
                 @Override
                 public void actionPerformed(ActionEvent e) {
                     opcion = Integer.parseInt(JOptionPane.showInputDialog("Que deséa hacer? (1-añadir mision/2-eliminar todas las misiones/3-lista de misiones;)"));
-                    switch (opcion){
+                    switch (opcion) {
                         case 1:
                             String nuevaMision = JOptionPane.showInputDialog("Inserte el nombre de la misión a realizar");
                             if (misiones.contains(nuevaMision)) {
-                                JOptionPane.showMessageDialog(null,"La misión ya existe en la base de datos.");
+                                JOptionPane.showMessageDialog(null, "La misión ya existe en la base de datos.");
                                 misiones.remove(2);
                             } else {
                                 misiones.add(nuevaMision);
                             }
-                        break;
+                            break;
                         case 2:
                             misiones.removeAll(misiones);
-                        break;
+                            break;
                         case 3:
-                            JOptionPane.showMessageDialog(null,"Las misiones actuales són las siguientes: "+misiones);
-                        break;
+                            JOptionPane.showMessageDialog(null, "Las misiones actuales són las siguientes: " + misiones);
+                            break;
                     }
                 }
             });
@@ -66,23 +67,51 @@ public class NASA extends JFrame {
                 @Override
                 public void actionPerformed(ActionEvent e) {
 
-                    
+                    // Definir la secuencia de signos para simular la trayectoria
+                    String trayectoria = generarTrayectoria(10); // Cambia el número para cambiar la longitud de la trayectoria
 
+                    // Mostrar la trayectoria en un JOptionPane
+                    JOptionPane.showMessageDialog(null, trayectoria, "Trayectoria Simulada", JOptionPane.PLAIN_MESSAGE);
+                }
+
+                // Método para generar una secuencia de signos para simular la trayectoria
+                public static String generarTrayectoria(int longitud) {
+                    // Generar coordenadas espaciales aleatorias
+                    String comunicado = "Fin de la simulación";
+                    double coordenadaX = Math.random() * 2000 - 1000;
+                    double coordenadaY = Math.random() * 2000 - 1000;
+                    double coordenadaZ = Math.random() * 2000 - 1000;
+
+                    // Generar una dirección aleatoria
+                    String[] direcciones = {"Norte", "Sur", "Este", "Oeste", "Arriba", "Abajo"};
+                    int indiceDireccion = (int) (Math.random() * direcciones.length);
+                    String direccion = direcciones[indiceDireccion];
+
+                    // Construir el mensaje para mostrar en el JOptionPane
+                    String mensaje = "Coordenadas Espaciales:\n" +
+                            "X: " + coordenadaX + "\n" +
+                            "Y: " + coordenadaY + "\n" +
+                            "Z: " + coordenadaZ + "\n\n" +
+                            "Dirección: " + direccion;
+
+                    // Mostrar el mensaje en un JOptionPane
+                     JOptionPane.showMessageDialog(null, mensaje, "Trayectoria Espacial", JOptionPane.INFORMATION_MESSAGE);
+                    return comunicado;
                 }
             });
             //Ver Reportes de mision
             MisionReport.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
-                    String MensajeLeido="";
+                    String MensajeLeido = "";
                     try {
 
                         FileWriter fw = new FileWriter("ReporteMision.txt");
 
-                        fw.write("1. La nave recorrerá una cantidad de 156 años luz  "+
-                        "2. El destino de la nave es el planeta de Cadia   "+
-                        "3. El objetivo de la misión es cerrar la estructura paranormal conocida como 'ojo del terror'   "+
-                         "FIN DEL REGISTRO DE LA MISION: "+misiones);
+                        fw.write("1. La nave recorrerá una cantidad de 156 años luz  " +
+                                "2. El destino de la nave es el planeta de Cadia   " +
+                                "3. El objetivo de la misión es cerrar la estructura paranormal conocida como 'ojo del terror'   " +
+                                "FIN DEL REGISTRO DE LA MISION: " + misiones);
 
                         fw.close();
                     } catch (IOException ex) {
@@ -96,9 +125,10 @@ public class NASA extends JFrame {
                         BufferedReader BR = new BufferedReader(lector);
                         MensajeLeido = BR.readLine();
 
-                    } catch (Exception ex) {}
+                    } catch (Exception ex) {
+                    }
 
-                    JOptionPane.showMessageDialog(null,MensajeLeido);
+                    JOptionPane.showMessageDialog(null, MensajeLeido);
                 }
             });
             // Ajustar el tamaño del JFrame y hacerlo visible
@@ -106,5 +136,4 @@ public class NASA extends JFrame {
             setVisible(true);
         }
     }
-
 }
